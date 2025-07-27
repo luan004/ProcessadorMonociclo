6af0e23be94d01c6b40bd69a2e749c27c845b675
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2025-07-27 16:23:56 -0300

    feat: classes principais

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Model.Interpreter;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste teste do luan");
+
     }
 }
