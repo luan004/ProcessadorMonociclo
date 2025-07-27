f58841f3a5bee00338c5317d6290cde0cc3a245f
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-07-27 20:06:58 -0300

    class memory

--- src/Model/Memory.java
+++ src/Model/Memory.java
@@ -1,4 +1,26 @@
 package Model;
 
 public class Memory {
+    
+    private static final int size = 1024;
+    private static short[] memory = new short[size];
+    
+    public static short read (short address){
+        return memory[address];
+    }
+    
+    public static void write (short address, short value){
+        memory[address] = value;
+    }
+    
+    public static int size (){
+        return  size;
+    }
+    
+    public static void printMemory(){
+        for (int i = 0; i<= 100; i++){
+            System.out.println("");
+        }
+    }
+    
 }
